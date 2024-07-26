80a16a19cb4195a246464d3778a1e8a50974ee5b
JRamos29 <malebolge@gmail.com>
2024-07-25 23:18:50 -0300

    Modified PsnAuth model;

--- src/models/psnAuth.ts
+++ src/models/psnAuth.ts
@@ -1,9 +1,4 @@
-export interface PsnAuth {
-  authTokenResponse: AuthTokenResponse;
-  accountId: string;
-}
-
-export interface AuthTokenResponse {
+interface IPsnAuth {
   /** Used to retrieve data from the PSN API. */
   accessToken: string;
   /** When the access token will expire. */
@@ -15,4 +10,55 @@ export interface AuthTokenResponse {
   refreshTokenExpiresIn: number;
   scope: string;
   tokenType: string;
+  //User accountId
+  accountId: string;
 }
+
+export type IPsnAuthTokensResponse = Omit<IPsnAuth, "accountId">;
+
+export class PsnAuth implements IPsnAuth {
+  accessToken: string;
+  expiresIn: number;
+  idToken: string;
+  refreshToken: string;
+  refreshTokenExpiresIn: number;
+  scope: string;
+  tokenType: string;
+  accountId: string;
+
+  constructor(
+    accessToken: string = "",
+    expiresIn: number = 0,
+    idToken: string = "",
+    refreshToken: string = "",
+    refreshTokenExpiresIn: number = 0,
+    scope: string = "",
+    tokenType: string = "",
+    accountId: string = ""
+  ) {
+    (this.accessToken = accessToken),
+      (this.expiresIn = expiresIn),
+      (this.idToken = idToken),
+      (this.refreshToken = refreshToken),
+      (this.refreshTokenExpiresIn = refreshTokenExpiresIn),
+      (this.scope = scope),
+      (this.tokenType = tokenType),
+      (this.accountId = accountId);
+  }
+}
+
+export const createPsnAuth = (
+  iPsnAuthTokensResponse: IPsnAuthTokensResponse,
+  accountId: string
+): PsnAuth => {
+  return new PsnAuth(
+    iPsnAuthTokensResponse.accessToken,
+    iPsnAuthTokensResponse.expiresIn,
+    iPsnAuthTokensResponse.idToken,
+    iPsnAuthTokensResponse.refreshToken,
+    iPsnAuthTokensResponse.refreshTokenExpiresIn,
+    iPsnAuthTokensResponse.scope,
+    iPsnAuthTokensResponse.tokenType,
+    accountId
+  );
+};
