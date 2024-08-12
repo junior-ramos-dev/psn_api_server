22ab5c77ce3475570590c49d04713d6154394af8
JRamos29 <malebolge@gmail.com>
2024-08-12 16:17:42 -0300

    Updated config files;

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,57 @@
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+import pluginJs from "@eslint/js";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    settings: {},
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      semi: "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { ignoreRestSiblings: true },
+      ],
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // `react` first, `next` second, then packages starting with a character
+            ["^react$", "^next", "^[a-z]"],
+            // Packages starting with `@`
+            ["^@"],
+            // Packages starting with `~`
+            ["^~"],
+            // Imports starting with `../`
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            // Imports starting with `./`
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            // Style imports
+            ["^.+\\.s?css$"],
+            // Side effect imports
+            ["^\\u0000"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    },
+  },
+  { ignores: ["node_modules/", "dist/", "**/temp/*"] },
 ];
