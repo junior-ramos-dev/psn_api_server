8197d4ca230125cf01883c63d09eecc86b43c33c
JRamos29 <malebolge@gmail.com>
2024-09-28 17:05:22 -0300

    Implemented register loader;

---
+++ src/services/loaders/auth/registerLoader.ts
@@ -0,0 +1,190 @@
+import { generateToken } from "@/controllers/authController";
+import { TaskHandler } from "@/models/classes/taskHandler";
+import { controllersErrorHandler } from "@/models/interfaces/common/error";
+import { IUserAndProfile, IUserProfile } from "@/models/interfaces/user";
+
+import { TaskFunctionParams } from "../../../models/types/taskHandler";
+import { PsnAuth } from "../../psnApi/psnAuth";
+
+import {
+  checkEmailExists,
+  checkOnlineIdExists,
+  createUserAndProfile,
+  getGamesTrophiesList,
+  getPsnCredentials,
+  getUserGamesList,
+  isMissingNpsso,
+  loadGamesIcons,
+} from "./registerLoaderSteps";
+
+// PsnAuth object
+let PSN_AUTH2: PsnAuth;
+
+interface RegisterData {
+  user: {
+    id: string;
+    psnOnlineId: string;
+    email: string;
+  };
+  profile: IUserProfile | undefined;
+}
+
+const registerData: RegisterData = {
+  user: {
+    id: "",
+    psnOnlineId: "",
+    email: "",
+  },
+  profile: undefined,
+};
+
+const setRegisterData = (
+  userData: IUserAndProfile,
+  registerData: RegisterData
+) => {
+  registerData.user.id = String(userData.userDb._id);
+  registerData.user.psnOnlineId = userData.userDb.psnOnlineId;
+  registerData.user.email = userData.userDb.email;
+  registerData.profile = userData.userProfileDb;
+};
+
+let runSubTask = 0;
+let stepId = 1;
+
+/**
+ * Register an user
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
+const registerUserLoader = async (
+  params: TaskFunctionParams,
+  taskHandler: TaskHandler
+) => {
+  // Subtasks Vars
+  // Step 5 - Create user and user profile
+  let userData: IUserAndProfile | undefined;
+  // Steps 6 to 8 - Load games and trophies
+  // let userId: string;
+
+  try {
+    console.log("TASK RUNNING");
+    console.log("x", taskHandler.logTaskProps());
+
+    // Step 1 - Check PSN onlineId exists
+    if (stepId === 1 && !runSubTask) {
+      runSubTask = 1;
+      await checkOnlineIdExists(params.psnOnlineId).then(() => {
+        stepId = 2;
+        runSubTask = 0;
+      });
+    }
+
+    // Step 2 - Check email
+    if (stepId === 2 && !runSubTask) {
+      runSubTask = 1;
+      await checkEmailExists(params.email).then(() => {
+        stepId = 3;
+        runSubTask = 0;
+      });
+    }
+
+    // Step 3 - Check if NPSSO exists
+    if (stepId === 3 && !runSubTask) {
+      runSubTask = 1;
+      //[return taskHandler error]
+      isMissingNpsso(params.npsso);
+
+      stepId = 4;
+      runSubTask = 0;
+    }
+
+    // Step 4 - Get PSN credentials
+    if (stepId === 4 && !runSubTask) {
+      runSubTask = 1;
+      getPsnCredentials(params.npsso).then((data) => {
+        PSN_AUTH2 = data;
+        stepId = 5;
+        runSubTask = 0;
+      });
+    }
+
+    // Step 5 - Create user and user profile
+    if (stepId === 5 && !runSubTask) {
+      runSubTask = 1;
+      // Create user and user profile
+      await createUserAndProfile(
+        params.psnOnlineId,
+        params.email,
+        params.password
+      ).then((data) => {
+        userData = data;
+        setRegisterData(userData, registerData);
+        stepId = 6;
+        runSubTask = 0;
+      });
+    }
+
+    // Steps 6 to 8 - Load games and trophies
+    // Step 6 - Get user games list
+    if (stepId === 6 && runSubTask) {
+      console.log("Running Step 6 - Get user games list...");
+    } else if (stepId === 6 && !runSubTask) {
+      runSubTask = 1;
+      await getUserGamesList(registerData.user.id).then(() => {
+        stepId = 7;
+        runSubTask = 0;
+      });
+    }
+    // Step 7 - Load the games icons
+    if (stepId === 7 && runSubTask) {
+      console.log("Running Step 7 - Load the games icons...");
+    } else if (stepId === 7 && !runSubTask) {
+      runSubTask = 1;
+      await loadGamesIcons(registerData.user.id).then(() => {
+        stepId = 8;
+        runSubTask = 0;
+      });
+    }
+
+    // Step 8 - Get the games trohies list
+    if (stepId === 8 && runSubTask) {
+      console.log("Step 8 - Get the games trohies list...");
+    } else if (stepId === 8 && !runSubTask) {
+      runSubTask = 1;
+      await getGamesTrophiesList(registerData.user.id).then(() => {
+        stepId = 9;
+        runSubTask = 0;
+      });
+    }
+
+    // Step 9 - Finishing (Return register data)
+    if (stepId === 9 && !runSubTask) {
+      runSubTask = 1;
+      generateToken(params.res, registerData.user.id);
+
+      taskHandler.data = registerData;
+
+      taskHandler.setTaskPropsRunTask(0);
+      stepId = 0;
+      runSubTask = 0;
+    }
+
+    taskHandler.setTaskPropsStepId(stepId);
+    return taskHandler.getResponse();
+  } catch (error) {
+    console.log(error);
+    const resObj = controllersErrorHandler(error);
+
+    taskHandler.error = {
+      status: resObj.status,
+      name: resObj.name,
+      message: resObj.message,
+      errors: resObj.errors ? resObj.errors : undefined,
+    };
+    return taskHandler.getResponse();
+  }
+};
+
+export { PSN_AUTH2, registerUserLoader };
---
+++ src/services/loaders/auth/registerLoaderSteps.ts
@@ -0,0 +1,128 @@
+import { IBulkResponse } from "@/models/interfaces/common/bulk";
+import { MongoDbError, PsnApiError } from "@/models/interfaces/common/error";
+import { PsnAuth } from "@/services/psnApi/psnAuth";
+import { upsertTrophiesForAllGamesBulk } from "@/services/repositories/bulk/trophy";
+import {
+  createDbGameIconBin,
+  createDbGamesByUser,
+} from "@/services/repositories/gameRepository";
+import {
+  createDbUserAndProfile,
+  getDbUserByEmail,
+  getDbUserByPsnOnlineId,
+} from "@/services/repositories/userRepository";
+
+// Step 1 - Check PSN onlineId exists
+export const checkOnlineIdExists = async (psnOnlineId: string) => {
+  const onlineIdExists = await getDbUserByPsnOnlineId(psnOnlineId);
+
+  if (onlineIdExists) {
+    //[return taskHandler error]
+    throw new MongoDbError(
+      `An account with PSN Username '${psnOnlineId}' already exists! If you used this username to register, try to Login.`
+    );
+  }
+};
+
+// Step 2 - Check email
+export const checkEmailExists = async (email: string) => {
+  const userEmailExists = await getDbUserByEmail(email);
+  if (userEmailExists) {
+    //[return taskHandler error]
+    throw new MongoDbError(
+      `An account with email '${email}' already exists! If you used this email to register, try to Login.`
+    );
+  }
+};
+
+// Step 3 - Check if NPSSO exists [return taskHandler error]
+export const isMissingNpsso = (npsso: string) => {
+  if (!npsso) {
+    throw new PsnApiError(
+      "An error occurred in creating the account: Missing 'NPSSO' code"
+    );
+  }
+};
+
+// Step 4 - Get PSN credentials
+export const getPsnCredentials = async (npsso: string) => {
+  const psnAuth = await PsnAuth.createPsnAuth(npsso).then((psnAuth) => psnAuth);
+
+  return psnAuth;
+};
+
+// Step 5 - Create user and user profile
+export const createUserAndProfile = async (
+  psnOnlineId: string,
+  email: string,
+  password: string
+) => {
+  // Create user and user profile
+  const data = await createDbUserAndProfile(psnOnlineId, email, password).then(
+    (data) => {
+      console.log("userData");
+      console.log(data?.userDb._id);
+      return data;
+    }
+  );
+
+  if (data && "userDb" in data && "userProfileDb" in data) {
+    return data;
+  } else {
+    // [return taskHandler error]
+    throw new MongoDbError("An error occurred while creating the account");
+  }
+};
+
+// Step 6 - Get user games list
+export const getUserGamesList = async (userId: string) => {
+  // Get the list of games from PSN and insert on DB
+  console.log(
+    `[${new Date().toISOString()}] Started loading games data from PSN...`
+  );
+
+  await createDbGamesByUser(userId)
+    .then((data) => {
+      console.log(
+        "================================================== GAMES LIST"
+      );
+      return data;
+    })
+    .catch(() => {
+      // [return taskHandler error]
+      throw new MongoDbError("An error occurred while getting the games list");
+    });
+};
+
+// Step 7 - Load the games icons
+export const loadGamesIcons = async (userId: string) => {
+  // Download and create (if not exists yet) the game image (trophyTitleIconUrl)
+  // and insert as binary data in the collection "gamesicons"
+  await createDbGameIconBin(userId).then(() => {
+    console.log(
+      "================================================== GAMES ICONS"
+    );
+  });
+};
+
+// Step 8 - Get the games trohies list
+export const getGamesTrophiesList = async (userId: string) => {
+  // Response for getting list of trophies
+  const bulkResponse: IBulkResponse<string> = {
+    name: "createTrophiesListForAllGamesBulk",
+    message: "",
+    data: {},
+    isError: false,
+  };
+
+  //Insert or Update the list of trophies for all games from a user (bulk)
+  await upsertTrophiesForAllGamesBulk(userId, bulkResponse).then(() => {
+    console.log(
+      "================================================== GAMES TROPHIES"
+    );
+  });
+
+  console.log(
+    `[${new Date().toISOString()}] Finished loading user data from PSN...`
+  );
+};
